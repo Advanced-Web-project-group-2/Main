b723e8d0aaac88eec73d1f1b57a8b9bb033ddc9b
rusty-Backend <petteri_patsi@hotmail.com>
2025-11-04 20:18:24 +0000

    Swagger documentation initialized

--- moovie/server/index.js
+++ moovie/server/index.js
@@ -1,6 +1,9 @@
 import express from 'express'
 import cors from 'cors'
 import pkg from 'pg'
+import swaggerUi from 'swagger-ui-express';
+import swaggerJsdoc from 'swagger-jsdoc';
+
 
 const port = 3001
 const { Pool }  = pkg
@@ -19,6 +22,54 @@ const openDb = () => {
     return pool
 }
 
+const swaggerOptions = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Moovie API',
+      version: '1.0.0',
+      description: 'API documentation for the Moovie backend',
+    },
+    servers: [
+      { url: `http://localhost:${port}` },
+    ],
+  },
+  apis: ['./index.js'],
+}
+
+const swaggerSpec = swaggerJsdoc(swaggerOptions)
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
+
+app.get('/api-docs.json', (req, res) => {
+  res.setHeader('Content-Type', 'application/json')
+  res.send(swaggerSpec)
+})
+
+/**
+ * @openapi
+ * /:
+ *   get:
+ *     summary: Get all users
+ *     responses:
+ *       200:
+ *         description: List of all users from db
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   id:
+ *                     type: integer
+ *                   username:
+ *                     type: string
+ *                   credits:
+ *                     type: number
+ *                   icon:
+ *                     type: string
+ */
+
 
 app.get('/', (req, res) => {
     const pool = openDb()
@@ -31,10 +82,32 @@ app.get('/', (req, res) => {
     })
 })
 
-
-app.listen(port, () => {
- console.log(`Server is running on http://localhost:${port}`)
-})
+/**
+ * @openapi
+ * /add-user:
+ *   post:
+ *     summary: Add a new user
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               username:
+ *                 type: string
+ *               passhash:
+ *                 type: string
+ *               credits:
+ *                 type: number
+ *               icon:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: User successfully created
+ *       500:
+ *         description: Server error
+ */
 
 app.post('/add-user', express.json(), (req, res) => {
   const pool = openDb()
@@ -49,3 +122,8 @@ app.post('/add-user', express.json(), (req, res) => {
     }
   )
 })
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`)
+  console.log(`Swagger documentation at http://localhost:${port}/api-docs`)
+});
