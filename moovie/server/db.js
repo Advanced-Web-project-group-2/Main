ed7a9d43473752e54bc931adbd9d23805dc69de6
tecnopistacho <pilar.bl.mp@gmail.com>
2025-11-13 12:23:27 +0200

    Working backend/frontend

--- moovie/server/db.js
+++ moovie/server/db.js
@@ -1,20 +1,12 @@
 import pkg from 'pg';
-import 'dotenv/config';
 const { Pool } = pkg;
 
 const pool = new Pool({
   user: process.env.PGUSER,
-  host: process.env.PGHOST,  
+  host: process.env.PGHOST,
   database: process.env.PGDATABASE,
   password: process.env.PGPASSWORD,
-  port: process.env.PGPORT,
-});
-
-
-pool.connect((err, client, release) => {
-  if (err) return console.error('Error acquiring client', err.stack);
-  console.log('Connected to PostgreSQL successfully!');
-  release();
+  port: parseInt(process.env.PGPORT),
 });
 
 export default pool;
