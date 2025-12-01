a80c31b17f4e765a8abf2fb1e2a7ccaabecb119a
Saattaj4 <markku.putaala@icloud.com>
2025-12-01 10:40:27 +0200

    bingbong added to devMain. Tested and working.

--- moovie/client/src/App.jsx
+++ moovie/client/src/App.jsx
@@ -5,6 +5,7 @@ import Layout from "./components/Layout.jsx";
 import Main from "./pages/Main.jsx";
 import Groups from "./pages/Groups.jsx";
 import GroupPage from "./pages/GroupPage.jsx";
+import CreateGroup from "./pages/CreateGroup.jsx";
 import InCinemas from "./pages/InCinemas.jsx";
 import AdvancedSearch from "./pages/AdvancedSearch.jsx";
 import SignUp from "./pages/SignUp.jsx";
@@ -22,7 +23,9 @@ export default function App() {
         <Route element={<Layout />}>
           <Route path="/" element={<Main />} />
           <Route path="/groups" element={<Groups />} />
+          <Route path="/groups/create" element={<CreateGroup />} />
           <Route path="/groups/:groupId" element={<GroupPage />} />
+          <Route path="/group/:groupId" element={<GroupPage />} />
           <Route path="/in-cinemas" element={<InCinemas />} />
           <Route path="/advanced-search" element={<AdvancedSearch />} />
           <Route path="/profile" element={<Profile />} />
@@ -38,11 +41,9 @@ export default function App() {
             path="/lists/favourites/public/:userId"
             element={<PublicFavourites />}
           />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/signin" element={<SignIn />} />
         </Route>
-
-        {/* Auth pages WITHOUT the layout */}
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/signin" element={<SignIn />} />
       </Routes>
     </Router>
   );
