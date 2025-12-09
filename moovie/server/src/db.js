fcf41fb70ab73f1ac436b18d01abbec0852927ea
Saattaj4 <markku.putaala@icloud.com>
2025-12-09 10:46:15 +0200

    debug messages

--- moovie/server/src/db.js
+++ moovie/server/src/db.js
@@ -1,6 +1,14 @@
 import pkg from 'pg';
 const { Pool } = pkg;
 
+console.log('Database config:', {
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD ? '***' : 'NOT SET',
+  port: process.env.PGPORT,
+});
+
 const pool = new Pool({
   user: process.env.PGUSER,
   host: process.env.PGHOST,
