960399b315ac5ed6a4ed65347d7deb3815b25097
Yvonne Frankort <yvonnefrankort@gmx.de>
2025-12-14 18:32:07 +0200

    Favorites working

--- moovie/client/src/services/listService.js
+++ moovie/client/src/services/listService.js
@@ -36,4 +36,50 @@ export async function createList(payload) {
   return json; // { list: {...} }
 }
 
-export default { getUserLists, addMovieToList, createList };
+// ---------------- FAVOURITES ----------------
+
+export async function getFavourites() {
+  const res = await fetch(`${API_ROOT}/favourites`, {
+    headers: authHeaders(),
+  });
+  if (!res.ok) throw new Error(await res.text());
+  return res.json(); // { favourites: [...] }
+}
+
+export async function addFavourite(movie) {
+  const res = await fetch(`${API_ROOT}/favourites`, {
+    method: 'POST',
+    headers: authHeaders(),
+    body: JSON.stringify({
+      movieId: movie.id,
+      movieName: movie.title || movie.name,
+      genre: movie.genres
+        ? movie.genres.map((g) => g.name).join(', ')
+        : movie.genre || null,
+      releaseYear: movie.release_date
+        ? movie.release_date.split('-')[0]
+        : movie.releaseYear || null,
+      posterUrl: movie.poster_path
+        ? `https://image.tmdb.org/t/p/w300${movie.poster_path}`
+        : movie.posterUrl || null,
+    }),
+  });
+
+  const json = await res.json();
+  if (!res.ok) throw new Error(json?.error || 'Failed to add to favourites');
+  return json;
+}
+
+export async function removeFavourite(movieId) {
+  const res = await fetch(`${API_ROOT}/favourites`, {
+    method: 'DELETE',
+    headers: authHeaders(),
+    body: JSON.stringify({ movieId }),
+  });
+
+  const json = await res.json();
+  if (!res.ok) throw new Error(json?.error || 'Failed to remove favourite');
+  return json;
+}
+
+export default { getUserLists, addMovieToList, createList, getFavourites, addFavourite, removeFavourite,};
